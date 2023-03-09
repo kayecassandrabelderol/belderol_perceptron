f5e5540bc1444ff81030f521dddab710c4b73c64
Kaye Cassandra Belderol <kayecassandra.belderol@cit.edu>
2023-03-09 17:37:55 +0800

    Create Project

---
+++ belderol_perceptronproj/Perceptron.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace belderol_perceptronproj
+{
+    internal class Perceptron
+    {
+    }
+}
