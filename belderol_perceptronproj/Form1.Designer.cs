083a284bd494bd18449864e58818c0aaf72883bc
Kaye Cassandra Belderol <kayecassandra.belderol@cit.edu>
2023-03-10 01:34:25 +0800

    Rename FormDesigner

--- belderol_perceptronproj/Form1.Designer.cs
+++ belderol_perceptronproj/Form1.Designer.cs
@@ -156,7 +156,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "OR Perceptron Algorithm";
             this.ResumeLayout(false);
             this.PerformLayout();
 
